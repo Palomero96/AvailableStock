47538f0b9f30eaf58fbec538690cc3fffb1bb0fa
David Palomero Méndez <31955373+Palomero96@users.noreply.github.com>
2021-02-15 19:07:17 +0100

    PcComp
    PcComponentes code and links done

--- AvailableStock.py
+++ AvailableStock.py
@@ -3,7 +3,11 @@ import os
 
 import pandas as pd
 import numpy as np
-from .check import check
+from check import check
+
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 __csv_dir = 'csv/'
 __products_file = 'csv/products.csv'
@@ -16,7 +20,30 @@ def main():
     #call scrap method
     #If available send email
 
+    print ("hello")
 
+
+
+def sendEmail(url):
+    port = 587  # For starttls
+    smtp_server = "smtp-mail.outlook.com"
+    sender_email = "palomero_96@hotmail.com"
+    receiver_email = "david.palomero.1996@gmail.com"
+    #Password 
+    password = ""
+    message = f"""\
+    Subject: Graphic card found
+
+    {url}"""
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        server.quit()
 
 
 if __name__ == '__main__':
--- check/pccomponentes.py
+++ check/pccomponentes.py
@@ -5,10 +5,22 @@ from bs4 import BeautifulSoup
 __default_header = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
 }
+parser='html.parser'
 
 #Scrap the page to check if it is available
-
 def check_pccomponentes(url):
+	# Getting the page
+	page = requests.get(url, headers=__default_header)
+	# Creating BeautifulSoup Object
+	soup = BeautifulSoup(page.content, parser)
 
+	allbuttons = soup.find_all("button", {"class": "buy-button"})
+	if not allbuttons:
+		#Return False if not available
+		return False
+	#Loop buttons to check if it is available
+	for button in allbuttons:
+		if "Comprar" in button.findChild().text:
+			return True
 	
 	return False
